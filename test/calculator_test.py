b44a1f145b3fc2d0579c807163a00d52f1bcf9bb
Rahul RK <rrangar@ncsu.edu>
2022-08-26 18:38:51 -0400

    added test cases

--- test/calculator_test.py
+++ test/calculator_test.py
@@ -1,11 +1,17 @@
 import pytest
-import os
-
-# print("Current directory ", os.path.curdir)
-
 from code.calculator import calculate
 
-def test_calculator():
+def test_addition():
     assert calculate(4, 2, '+') == 6 
 
-test_calculator()
+def test_subtraction():
+    assert calculate(4, 2, '-') == 2
+
+def test_multiplication():
+    assert calculate(4, 2, '*') == 8
+
+def test_division():
+    assert calculate(4, 2, '/') == 2.0
+
+def test_modulus():
+    assert calculate(4, 3, '%') == 1
